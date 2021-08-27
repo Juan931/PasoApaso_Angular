32b517d9d60e230428ba63df1dc7572568d20328
Juan Medina <juandavidds08@gmail.com>
2021-08-27 16:56:20 -0500

    Event Binding

--- src/app/components/componente/desboard/desboard.component.ts
+++ src/app/components/componente/desboard/desboard.component.ts
@@ -1,6 +1,7 @@
 import { templateJitUrl } from "@angular/compiler";
 import { Component } from "@angular/core";
 
+
 @Component({
  selector: "app-desboard",
  templateUrl: "./desboard.component.html",
@@ -11,17 +12,26 @@ export class DesboarComponent {
     nombre: string;
     img: string;
     texto: string;
+    title:boolean | string;
     constructor(){
         this.nombre= "Juan Medina"; //aca estamos creando nuestra variable despues vamos al html del componente
         this.img="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vaporwave-4K-Wallpapers.jpg/800px-Vaporwave-4K-Wallpapers.jpg"
         this.texto="Escribe aqui"; //esto es porperting binding
+        this.title="esto es event bindig da click para probar";
+
     }
     getName():string{
         return this.nombre=" juan medina 2"
     }
 
+
     mult(num1:number, num2:number) {
         return num1 * num2
 
     }
+
+    cambiarNombre():void {
+      this.title = "probando event binding"; // esto es event bindig
+    }
+   
 }
