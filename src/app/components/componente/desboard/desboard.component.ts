b6a7a35e498b761e41d80a53e8fc74dd442e5cc2
Juan Medina <juandavidds08@gmail.com>
2021-08-27 15:23:52 -0500

    interpolacion

--- src/app/components/componente/desboard/desboard.component.ts
+++ src/app/components/componente/desboard/desboard.component.ts
@@ -6,7 +6,19 @@ import { Component } from "@angular/core";
  templateUrl: "./desboard.component.html",
  styleUrls: ["./desboard.component.css"]
 })
-
+//interpolacion: desde el archivo ts es donde vamos a enviar las variables a nuestro componente html
 export class DesboarComponent {
+    nombre: string;
 
+    constructor(){
+        this.nombre= "Juan Medina"; //aca estamos creando nuestra variable despues vamos al html del componente
+    }
+    getName():string{
+        return this.nombre=" juan medina 2"
+    }
+
+    mult(num1:number, num2:number) {
+        return num1 * num2
+
+    }
 }
